671fd2357480c0fbf5ea05a88e2d62192c5751b6
AbdoSabir <168451366+AbdoSabir@users.noreply.github.com>
2024-11-19 13:52:12 +0100

    Create WebSocketListener.dart

---
+++ Dart/WebSocketListener.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+/// Custom Action: Listen to WebSocket Messages
+/// This action connects to a WebSocket server and updates the
+/// `texto` variable in FFState when a message is received.
+
+Future<void> listenToWebSocket(BuildContext context) async {
+    String wsUrl = "wss://websocket.accevent.com:8081";
+  String? initialMessage = "Flutterflow = connected  tt";
+  try {
+    // Create the WebSocket connection
+    final WebSocketChannel channel = WebSocketChannel.connect(Uri.parse(wsUrl));
+
+    // Send an initial message if provided
+    if (initialMessage != null) {
+      channel.sink.add(initialMessage);
+    }
+
+    // Listen for incoming messages
+    channel.stream.listen((message) {
+      // Update the global state variable to rebuild UI
+      // Update the global state variable using the update method
+      FFAppState().update(() {
+        FFAppState().texto = message;
+      });
+    }, onError: (error) {
+      print('WebSocket error: $error');
+    }, onDone: () {
+      print('WebSocket connection closed.');
+    });
+  } catch (e) {
+    print('Error connecting to WebSocket: $e');
+  }
+}
